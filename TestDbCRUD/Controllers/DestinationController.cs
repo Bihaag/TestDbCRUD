37728f3c34769f6092e96b48a2d48439a2c53c91
Bihaag <bihaagnepaul@gmail.com>
2023-09-27 18:47:55 +0200

    New Order Routes page with Google Maps

---
+++ TestDbCRUD/Controllers/DestinationController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TestDbCRUD.Data;
+using TestDbCRUD.Models;
+using TestDbCRUD.Models.Domain;
+
+namespace TestDbCRUD.Controllers
+{
+    public class DestinationController : Controller
+    {
+        private readonly MoveItDbContext moveItDbContext;
+
+        public DestinationController(MoveItDbContext moveItDbContext)
+        {
+            this.moveItDbContext = moveItDbContext;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Routes()
+        {
+            var orders = await moveItDbContext.Orders.ToListAsync();
+            return View(orders);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Maps()
+        {
+            return View();
+        }
+
+
+
+
+
+    }
+}
